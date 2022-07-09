5a7741596208d943ed48ecf097b18e01a7c5516f
Shahid Shafi <shahidshafi.org@gmail.com>
2022-07-09 11:13:33 +0530

    Updates

---
+++ SiteData/SiteData.js
@@ -0,0 +1,42 @@
+const grocery = [
+    {
+        id: 1,
+        brand: 'Pure Natural Honey',
+        imgSrc: '/images/honeyBottle-1.png',
+        price: 280,
+        discount: 36,
+        rating: 4,
+        numRatings: 100
+    },
+    {
+        id: 2,
+        brand: 'Raw Organic Honey',
+        imgSrc: '/images/honeyBottle-2.webp',
+        price: 250,
+        discount: 26,
+        rating: 4,
+        numRatings: 100
+    },
+    {
+        id: 3,
+        brand: 'Orara Valley Honey',
+        imgSrc: '/images/honeyBottle-3.jpg',
+        price: 180,
+        discount: 18,
+        rating: 4,
+        numRatings: 100
+    },
+    {
+        id: 4,
+        brand: 'Green Natural Honey',
+        imgSrc: '/images/honeyBottle-4.webp',
+        price: 150,
+        discount: 20,
+        rating: 4,
+        numRatings: 100
+    }
+];
+
+const electronic = [
+    
+]
